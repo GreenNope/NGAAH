8a9eb0f2c41e4881c1e3e17068c1db73c7c9f684
GreenNope <tlash1000@gmail.com>
2017-12-27 14:44:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,17 +4,20 @@ var thirdLetter = ["a", "k"]
 
 var number = 1;
 
-function newLink (i, j, k, l, m) {
-    var link = document.createElement
+function newLink (i, j, k, l, m, number) {
+    
+    var lineBreak = document.createElement("br");
+    var link = "goo.gl/" + i + j + k + l + m + "d";
+}
+    
 
 for (i = 0; i < eLetter.length; i++) {
     for (j = 0; j < secondLetter.length; j++) {
         for (k = 0; k < thirdLetter.length; k++) {
             for (l = 0; l < eLetter.length; l++) {
                 for (m = 0; m < eLetter.length; m++) {
-                    
-            
-            
+                    newlink(i, j, k, l, m, number);
+                    number = number + 1;
                 }
             }
         }
