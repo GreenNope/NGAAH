006a47b9468c43a44d2ae1ed3e3acc60e6c07193
GreenNope <tlash1000@gmail.com>
2017-12-27 14:35:26 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+var eLetter = ["B", "C", "D", "E", "G", "P", "T", "V", "Z"];
+var secondLetter = ["9", "i"]
+var thirdLetter = ["a", "k"]
+
+var number = 1;
+
+function newLink (i, j, k, l, m) {
+    var link = document.createElement
+
+for (i = 0; i < eLetter.length; i++) {
+    for (j = 0; j < secondLetter.length; j++) {
+        for (k = 0; k < thirdLetter.length; k++) {
+            for (l = 0; l < eLetter.length; l++) {
+                for (m = 0; m < eLetter.length; m++) {
+                    
+            
+            
+                }
+            }
+        }
+    }
+}
